b90cbf06f450a7f836f41f0e3752f944a1b50164
Gleb <glebplatonov08@gmail.com>
2024-04-13 12:14:39 +0300

    added login DB and form

---
+++ app/data/users.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    team = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def __repr__(self):
+        return f'<User> {self.id} {self.name} {self.email}'
